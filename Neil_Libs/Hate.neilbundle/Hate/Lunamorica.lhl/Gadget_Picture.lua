eea954f626b439b6057a3808e0b1e9c2fa6ac77c
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2022-07-18 18:18:40 +0200

    Removed dupe license block

--- Neil_Libs/Hate.neilbundle/Hate/Lunamorica.lhl/Gadget_Picture.lua
+++ Neil_Libs/Hate.neilbundle/Hate/Lunamorica.lhl/Gadget_Picture.lua
@@ -6,17 +6,7 @@
 -- Mozilla Public License, v. 2.0. If a copy of the MPL was not
 -- distributed with this file, You can obtain one at
 -- http://mozilla.org/MPL/2.0/.
--- Version: 22.07.15
---[[
-        Gadget_Picture.lua
-	(c) 2017 Jeroen Petrus Broks.
-	
-	This Source Code Form is subject to the terms of the 
-	Mozilla Public License, v. 2.0. If a copy of the MPL was not 
-	distributed with this file, You can obtain one at 
-	http://mozilla.org/MPL/2.0/.
-        Version: 17.11.07
-]]
+-- Version: 22.07.18
 -- </License Block>
 
 local pic = {
